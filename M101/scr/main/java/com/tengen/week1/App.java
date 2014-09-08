a929896ee4a163196d2057446ad02ab11a0bb37a
tanvi1gupta <tg11991@gmail.com>
2014-09-08 12:18:03 +0530

    Create App.java

---
+++ M101/scr/main/java/com/tengen/week1/App.java
@@ -0,0 +1,13 @@
+package com.tengen.week1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
